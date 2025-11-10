045738f311e0b456d104b71456638a861cf2a728
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-11-10 18:17:36 +0530

    Added the backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("heyy ");
+import express from "express";
+
+const app=express();
+app.get('/',(req,res)=>{
+    res.status(200).json({msg:"hello world"})
+});
+
+app.listen(3000,()=>console.log("server running on port 3000"));
